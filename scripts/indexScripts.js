f750754a36da727a1e47fe975b9e8d8366eebeae
Cole <ccoombs002@drury.edu>
2025-05-13 21:35:07 -0500

    Testing functionality of scripts

--- scripts/indexScripts.js
+++ scripts/indexScripts.js
@@ -1,12 +1,14 @@
-ScrollReveal().reveal('#hero', {
-  duration: 1000,
-  origin: 'top',
-  distance: '50px'
-});
+document.addEventListener("DOMContentLoaded", function () {
+  ScrollReveal().reveal('#hero', {
+    duration: 1000,
+    origin: 'top',
+    distance: '50px'
+  });
 
-ScrollReveal().reveal('#summary, #education, #certs, #experience, #skills, #projects, #achievements', {
-  duration: 800,
-  origin: 'bottom',
-  distance: '30px',
-  interval: 200
+  ScrollReveal().reveal('#summary, #education, #certs, #experience, #skills, #projects, #achievements', {
+    duration: 800,
+    origin: 'bottom',
+    distance: '30px',
+    interval: 200
+  });
 });
