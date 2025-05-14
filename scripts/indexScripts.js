5c54cd6422c421579c7cda7e21916eb24b9b455e
Cole <ccoombs002@drury.edu>
2025-05-13 21:02:13 -0500

    Template down

---
+++ scripts/indexScripts.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('#hero', {
+  duration: 1000,
+  origin: 'top',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('#summary, #education, #certs, #experience, #skills, #projects, #achievements', {
+  duration: 800,
+  origin: 'bottom',
+  distance: '30px',
+  interval: 200
+});
